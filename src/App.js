4179aa72d18af77c0d8437282a4805dae0281ed6
b0ink <40929320+b0ink@users.noreply.github.com>
2024-03-28 18:54:13 +1100

    highlight selected node onClick

--- src/App.js
+++ src/App.js
@@ -22,11 +22,44 @@ function App() {
     //     name: node.label, // Set the name to the node label
     // }));
 
-    
+    class Node {
+        constructor(id, lon, lat) {
+            this.id = id;
+            this.coordinates = { lon, lat };
+            this.neighbors = [];
+        }
+        addNeighbor(neighborNode) {
+            this.neighbors.push(neighborNode);
+        }
+    }
 
+    class Path {
+        constructor(startNode, endNode, cost) {
+            this.startNode = startNode;
+            this.endNode = endNode;
+            this.cost = cost;
+        }
+    }
+
+    function AddNewPath(node1, node2, color = "red", label = "") {
+        console.log(node1);
+        let newTrace = {
+            type: "scattermapbox",
+            mode: "lines",
+            lon: [node1.coordinates.lon, node2.coordinates.lon],
+            lat: [node1.coordinates.lat, node2.coordinates.lat],
+            line: {
+                width: 2,
+                color: color,
+            },
+            name: label,
+            showLegend: false,
+        };
+        return newTrace;
+    }
 
     // const layout = {
-      const [layout, setLayout] = useState({
+    const [layout, setLayout] = useState({
         autosize: false,
         width: 1200,
         height: 900,
@@ -46,25 +79,6 @@ function App() {
     });
 
     // const [figure, setFigure] = useState({ data: [], layout: layout });
-
-    class Node {
-        constructor(id, lon, lat) {
-            this.id = id;
-            this.coordinates = { lon, lat };
-            this.neighbors = [];
-        }
-        addNeighbor(neighborNode) {
-            this.neighbors.push(neighborNode);
-        }
-    }
-
-    class Path {
-        constructor(startNode, endNode, cost) {
-            this.startNode = startNode;
-            this.endNode = endNode;
-            this.cost = cost;
-        }
-    }
 
     var gNodes = [];
     var gPaths = [];
@@ -139,18 +153,38 @@ function App() {
         setToken(true);
 
         // https://stackoverflow.com/questions/77977461/updating-single-data-point-in-react-plotly-without-re-rendering-entire-plot
-        setTimeout(() => {
-            let index = 0;
-            for (let trace of nodeTraces) {
-                trace.marker.color = "red";
-                index++;
-                if (index > 7) {
-                    break;
-                }
-            }
-            // setRevision(revision+1); 
-            setTraces([...nodeTraces]);
-        }, 5000);
+        // setTimeout(() => {
+        //     let index = 0;
+        //     for (let trace of nodeTraces) {
+        //         trace.marker.color = "red";
+        //         index++;
+        //         if (index > 7) {
+        //             break;
+        //         }
+        //     }
+
+        //     // setRevision(revision+1);
+        //     setTraces([...nodeTraces]);
+
+        //     let newData = []
+
+        //     for(let i = 1000; i<=5000;i+= 1000){
+        //       setTimeout(()=>{
+        //         for(let j = 0; j < gNodes.length-1;j++){
+        //           if(Math.random() < 0.05){
+        //             newData.push(AddNewPath(gNodes[j], gNodes[j+1]));
+        //           }
+        //         }
+        //         setTraces([...nodeTraces, ...newData]);
+        //         newData = []
+        //       },i)
+        //     }
+
+        //     setTimeout(()=>{
+        //       // clears paths
+        //       setTraces([...nodeTraces]);
+        //     }, 10000);
+        // }, 5000);
     };
     // let dataRetrieved = false;
 
@@ -163,9 +197,22 @@ function App() {
     }, [token]);
     // getData();
 
-    const onClick = (e) =>{
-      console.log(e)
-    }
+    const onClick = (e) => {
+        console.log(e.points[0]);
+        console.log(traces);
+
+        let index = e.points[0].fullData.index;
+
+        for (let i = 0; i < traces.length; i++) {
+            if (i == index) {
+                traces[i].marker.color = "red";
+            } else {
+                traces[i].marker.color = "blue";
+            }
+        }
+        setTraces([...traces])
+    };
+
     return (
         <div className="App">
             <Plot
